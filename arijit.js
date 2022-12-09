8144939c105b554a9672a8314977f25eb942207a
Shekhar <shekharhans161@gmail.com>
2022-12-09 06:35:33 +0530

    -a

--- arijit.js
+++ arijit.js
@@ -1,110 +1,303 @@
+const music = new Audio('/songs/yeh fitoor.mp3');
 
-// **PLay music 
-let songIndex=0;
-let audioElement = new Audio('./songs/1.mp3');
-let masterPlay=document.getElementById('masterPlay');
-let masterSongName=document.getElementById('masterSongName');
-let myProgressBar=document.getElementById('myProgressBar');
-let gif=document.getElementById('gif');
-let songItem=Array.from(document.getElementsByClassName('songItem'));
+// create Array 
 
-let songs=[
-    {songName:"Bhalobashar Morshum", filePath:"songs/1.mp3",coverPath : " covers/arijit1.png"},
-    {songName:"Fitoor", filePath:"songs/2.mp3",coverPath : " covers/arijit2.png"},
-    {songName:"Ami Je Tomar", filePath:"songs/3.mp3",coverPath : " covers/arijit3.png"},
-    {songName:"Kalank", filePath:"songs/4.mp3",coverPath : " covers/arijit4.png"},
-    {songName:"Laal Ishq", filePath:"songs/5.mp3",coverPath : " covers/arijit5.png"},
+const songs = [
+    {
+        id:'1',
+        songName:` Bhalobashar Morshum <br>
+        <div class="subtitle">Arijit Singh & Shreya Ghoshal</div>`,
+        poster: "covers/1.jpg"
+    },
+    {
+        id:'2',
+        songName:` Fitoor <br>
+        <div class="subtitle">Arijit Singh and Neeti Mohan</div>`,
+        poster: "covers/2.jpg"
+    },
+    // all object type 
+    {
+        id:"3",
+        songName: `Ami Je Tomar <br><div class="subtitle">Arijit Singh & Shreya Ghoshal</div>`,
+        poster: "covers/3.jpg",
+    },
+    {
+        id:"4",
+        songName: `Kalank(Title Track) <br><div class="subtitle">Arijit Singh</div>`,
+        poster: "covers/4.jpg",
+    },
+    {
+        id:"5",
+        songName: `Laal Ishq <br><div class="subtitle">Arijit Singh</div>`,
+        poster: "covers/5.jpg",
+    },
+    {
+        id:"6",
+        songName: `Hawayein <br><div class="subtitle">Arijit Singh</div>`,
+        poster: "covers/6.jpg",
+    },
+    {
+        id:"7",
+        songName: `Raabta <br><div class="subtitle">Arijit Singh</div>`,
+        poster: "covers/7.jpg",
+    },
+    {
+        id:"8",
+        songName: `Mast Magan<br><div class="subtitle">Ariit Singh & Chinmayi</div>`,
+        poster: "covers/8.jpg",
+    },
+    {
+        id:"9",
+        songName: `Enna Sona<br><div class="subtitle">Arijit Singh</div>`,
+        poster: "covers/9.jpg",
+    },
+    {
+        id:"10",
+        songName: `Duniya <br><div class="subtitle">Luka Chuppi</div>`,
+        poster: "img/10.jpg",
+    },
+    {
+        id:"11",
+        songName: `Lagdi Lahore Di <br><div class="subtitle">Street Dancer 3D</div>`,
+        poster: "img/11.jpg",
+    },
+    {
+        id:"12",
+        songName: `Putt Jatt Da <br><div class="subtitle">Putt Jatt Da</div>`,
+        poster: "img/12.jpg",
+    },
+    {
+        id:"13",
+        songName: `Baarishein <br><div class="subtitle">Atif Aslam</div>`,
+        poster: "img/13.jpg",
+    },
+    {
+        id:"14",
+        songName: `Vaaste <br><div class="subtitle">Dhvani Bhanushali</div>`,
+        poster: "img/14.jpg",
+    },
+    {
+        id:"15",
+        songName: `Lut Gaye <br><div class="subtitle">Jubin Nautiyal</div>`,
+        poster: "img/15.jpg",
+    },
 ]
-songItem.forEach((element,i) => {
-    element.getElementsByTagName("img")[0].src=songs[i].coverPath;
-    element.getElementsByClassName("songName")[0].innerHTML=songs[i].songName;
-});
-// audioElement.play();
-// Handel Play/Pause click
+
+Array.from(document.getElementsByClassName('songItem')).forEach((element, i)=>{
+    element.getElementsByTagName('img')[0].src = songs[i].poster;
+    element.getElementsByTagName('h5')[0].innerHTML = songs[i].songName;
+})
+
+
+let masterPlay = document.getElementById('masterPlay');
+let wave = document.getElementsByClassName('wave')[0];
+
 masterPlay.addEventListener('click',()=>{
-    if (audioElement.paused||audioElement.currentTime<=0) {
-        audioElement.play();
-        masterPlay.classList.remove('fa-circle-play');
-        masterPlay.classList.add('fa-circle-pause')
-        gif.style.opacity=1;
+    if (music.paused || music.currentTime <=0) {
+        music.play();
+        masterPlay.classList.remove('bi-play-fill');
+        masterPlay.classList.add('bi-pause-fill');
+        wave.classList.add('active2');
+    } else {
+        music.pause();
+        masterPlay.classList.add('bi-play-fill');
+        masterPlay.classList.remove('bi-pause-fill');
+        wave.classList.remove('active2');
     }
-    else{
-        audioElement.pause();
-        masterPlay.classList.remove('fa-circle-pause')
-        masterPlay.classList.add('fa-circle-play');
-        gif.style.opacity=1;
-    }
-})
-// Listen to Events
-audioElement.addEventListener('timeupdate',()=>{
-// console.log('timeupdate');
+} )
 
 
-// Update Seekbar
-progress =parseInt((audioElement.currentTime/audioElement.duration)*100);
-myProgressBar.value =progress;
-})
-myProgressBar.addEventListener('change',()=>{
-    audioElement.currentTime=myProgressBar.value *audioElement.duration/100;
-})
-const makeAllPlays =()=>{
-    Array.from(document.getElementsByClassName('songPlayBtn')).forEach((element)=>{
-        element.classList.remove('fa-circle-pause');
-        element.classList.add('fa-circle-play');
+const makeAllPlays = () =>{
+    Array.from(document.getElementsByClassName('playListPlay')).forEach((element)=>{
+            element.classList.add('bi-play-circle-fill');
+            element.classList.remove('bi-pause-circle-fill');
     })
 }
-Array.from (document.getElementsByClassName('songPlayBtn')).forEach((element)=>{
-    element.addEventListener('click',(e)=>{
+const makeAllBackgrounds = () =>{
+    Array.from(document.getElementsByClassName('songItem')).forEach((element)=>{
+            element.style.background = "rgb(105, 105, 170, 0)";
+    })
+}
+
+let index = 0;
+let poster_master_play = document.getElementById('poster_master_play');
+let title = document.getElementById('title');
+Array.from(document.getElementsByClassName('playListPlay')).forEach((element)=>{
+    element.addEventListener('click', (e)=>{
+        index = e.target.id;
         makeAllPlays();
-        gif.style.opacity=1;
+        e.target.classList.remove('bi-play-circle-fill');
+        e.target.classList.add('bi-pause-circle-fill');
+        music.src = `songs/${index}.mp3`;
+        poster_master_play.src =`covers/${index}.jpg`;
+        music.play();
+        let song_title = songs.filter((ele)=>{
+            return ele.id == index;
+        })
 
-        masterSongName.innerText=songs[songIndex].songName;
-        songIndex=parseInt(e.target.id);
-        e.target.classList.remove('fa-circle-play');
-        e.target.classList.add('fa-circle-pause');
-        audioElement.src=`songs/${songIndex}.mp3`;
-        audioElement.currentTime=0;
-        audioElement.play();
-        masterPlay.classList.remove('fa-circle-play');
-        masterPlay.classList.add('fa-circle-pause');
-
+        song_title.forEach(ele =>{
+            let {songName} = ele;
+            title.innerHTML = songName;
+        })
+        masterPlay.classList.remove('bi-play-fill');
+        masterPlay.classList.add('bi-pause-fill');
+        wave.classList.add('active2');
+        music.addEventListener('ended',()=>{
+            masterPlay.classList.add('bi-play-fill');
+            masterPlay.classList.remove('bi-pause-fill');
+            wave.classList.remove('active2');
+        })
+        makeAllBackgrounds();
+        Array.from(document.getElementsByClassName('songItem'))[`${index-1}`].style.background = "rgb(105, 105, 170, .1)";
     })
 })
-document.getElementById('previous').addEventListener('click',()=>{
-    if (songIndex<=0) {
-        songIndex=4;
-    }
-    else{
-        songIndex-=1;
-    }
-    audioElement.src=`songs/${songIndex}.mp3`;
-    masterSongName.innerText=songs[songIndex].songName;
-    gif.style.opacity=1;
 
-        audioElement.currentTime=0;
-        audioElement.play();
-        masterPlay.classList.remove('fa-circle-play');
-    masterPlay.classList.add('fa-circle-pause');
-})
-document.getElementById('next').addEventListener('click',()=>{
-    if (songIndex>=4) {
-        songIndex=0;
-    }
-    else{
-        songIndex+=1;
-    }
-    audioElement.src=`songs/${songIndex}.mp3`;
-    masterSongName.innerText=songs[songIndex].songName;
-    gif.style.opacity=1;
 
-        audioElement.currentTime=0;
-        audioElement.play();
-        masterPlay.classList.remove('fa-circle-play');
-    masterPlay.classList.add('fa-circle-pause');
+let currentStart = document.getElementById('currentStart');
+let currentEnd = document.getElementById('currentEnd');
+let seek = document.getElementById('seek');
+let bar2 = document.getElementById('bar2');
+let dot = document.getElementsByClassName('dot')[0];
+
+music.addEventListener('timeupdate',()=>{
+    let music_curr = music.currentTime;
+    let music_dur = music.duration;
+
+    let min = Math.floor(music_dur/60);
+    let sec = Math.floor(music_dur%60);
+    if (sec<10) {
+        sec = `0${sec}`
+    }
+    currentEnd.innerText = `${min}:${sec}`;
+
+    let min1 = Math.floor(music_curr/60);
+    let sec1 = Math.floor(music_curr%60);
+    if (sec1<10) {
+        sec1 = `0${sec1}`
+    }
+    currentStart.innerText = `${min1}:${sec1}`;
+
+    let progressbar = parseInt((music.currentTime/music.duration)*100);
+    seek.value = progressbar;
+    let seekbar = seek.value;
+    bar2.style.width = `${seekbar}%`;
+    dot.style.left = `${seekbar}%`;
 })
-$(document).ready(function(){
-    $('.container ul.toggle').click(function(){
-        $(this).toggleClass('active');
-        $('.container .sidebar').toggleClass('active');
+
+seek.addEventListener('change', ()=>{
+    music.currentTime = seek.value * music.duration/100;
+})
+
+music.addEventListener('ended', ()=>{
+    masterPlay.classList.add('bi-play-fill');
+    masterPlay.classList.remove('bi-pause-fill');
+    wave.classList.remove('active2');
+})
+
+
+let vol_icon = document.getElementById('vol_icon');
+let vol = document.getElementById('vol');
+let vol_dot = document.getElementById('vol_dot');
+let vol_bar = document.getElementsByClassName('vol_bar')[0];
+
+vol.addEventListener('change', ()=>{
+    if (vol.value == 0) {
+        vol_icon.classList.remove('bi-volume-down-fill');
+        vol_icon.classList.add('bi-volume-mute-fill');
+        vol_icon.classList.remove('bi-volume-up-fill');
+    }
+    if (vol.value > 0) {
+        vol_icon.classList.add('bi-volume-down-fill');
+        vol_icon.classList.remove('bi-volume-mute-fill');
+        vol_icon.classList.remove('bi-volume-up-fill');
+    }
+    if (vol.value > 50) {
+        vol_icon.classList.remove('bi-volume-down-fill');
+        vol_icon.classList.remove('bi-volume-mute-fill');
+        vol_icon.classList.add('bi-volume-up-fill');
+    }
+
+    let vol_a = vol.value;
+    vol_bar.style.width = `${vol_a}%`;
+    vol_dot.style.left = `${vol_a}%`;
+    music.volume = vol_a/100;
+})
+
+
+
+let back = document.getElementById('back');
+let next = document.getElementById('next');
+
+back.addEventListener('click', ()=>{
+    index -= 1;
+    if (index < 1) {
+        index = Array.from(document.getElementsByClassName('songItem')).length;
+    }
+    music.src = `songs/${index}.mp3`;
+    poster_master_play.src =`covers/${index}.jpg`;
+    music.play();
+    let song_title = songs.filter((ele)=>{
+        return ele.id == index;
     })
+
+    song_title.forEach(ele =>{
+        let {songName} = ele;
+        title.innerHTML = songName;
+    })
+    makeAllPlays()
+
+    document.getElementById(`${index}`).classList.remove('bi-play-fill');
+    document.getElementById(`${index}`).classList.add('bi-pause-fill');
+    makeAllBackgrounds();
+    Array.from(document.getElementsByClassName('songItem'))[`${index-1}`].style.background = "rgb(105, 105, 170, .1)";
+    
+})
+next.addEventListener('click', ()=>{
+    index -= 0;
+    index += 1;
+    if (index > Array.from(document.getElementsByClassName('songItem')).length) {
+        index = 1;
+        }
+    music.src = `songs/${index}.mp3`;
+    poster_master_play.src =`covers/${index}.jpg`;
+    music.play();
+    let song_title = songs.filter((ele)=>{
+        return ele.id == index;
+    })
+
+    song_title.forEach(ele =>{
+        let {songName} = ele;
+        title.innerHTML = songName;
+    })
+    makeAllPlays()
+
+    document.getElementById(`${index}`).classList.remove('bi-play-fill');
+    document.getElementById(`${index}`).classList.add('bi-pause-fill');
+    makeAllBackgrounds();
+    Array.from(document.getElementsByClassName('songItem'))[`${index-1}`].style.background = "rgb(105, 105, 170, .1)";
+    
+})
+
+
+let left_scroll = document.getElementById('left_scroll');
+let right_scroll = document.getElementById('right_scroll');
+let pop_song = document.getElementsByClassName('pop_song')[0];
+
+left_scroll.addEventListener('click', ()=>{
+    pop_song.scrollLeft -= 330;
+})
+right_scroll.addEventListener('click', ()=>{
+    pop_song.scrollLeft += 330;
+})
+
+
+let left_scrolls = document.getElementById('left_scrolls');
+let right_scrolls = document.getElementById('right_scrolls');
+let item = document.getElementsByClassName('item')[0];
+
+left_scrolls.addEventListener('click', ()=>{
+    item.scrollLeft -= 330;
+})
+right_scrolls.addEventListener('click', ()=>{
+    item.scrollLeft += 330;
 })
